95db6e6270bed34bd6160168675f100e8c53dc12
Ttoni4 <ttoni.email@gmail.com>
2021-09-26 21:11:33 +0500

    task6

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -3,19 +3,25 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
+        int numDigits = 0;
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        long sum = (long)x + (long)y;
 
-        return 0;
+        while(sum != 0){
+            numDigits += 1;
+            sum /= 10;
+        }
+
+        return numDigits == 0 ? 1 : numDigits;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int result = solution(12, 34);
+
+        int result = solution(-111, -222);
         System.out.println(result);
-        */
+
     }
 
 }
